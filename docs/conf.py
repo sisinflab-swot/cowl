17337a28140fa35a6b1155f50d87cc1995b64842
Ivano Bilenchi <ivanobilenchi@gmail.com>
2025-03-13 18:13:18 +0100

    Update docs configuration

--- docs/conf.py
+++ docs/conf.py
@@ -1,45 +1,41 @@
 # Project metadata
 
-project = '@PROJECT_NAME@'
-copyright = '@COWL_COPYRIGHT_YEAR@, @COWL_VENDOR@'
-author = '@COWL_VENDOR@'
-version = '@PROJECT_VERSION@'
-release = '@PROJECT_VERSION@'
-logo = '@COWL_ICON@'
-git_url = '@COWL_GIT_URL@'
-poliba_url = 'http://www.poliba.it'
-sisinflab_url = 'http://sisinflab.poliba.it'
-swot_url = '@COWL_VENDOR_URL@'
+project = "@PROJECT_NAME@"
+copyright = "@COWL_COPYRIGHT_YEAR@, @COWL_VENDOR@"
+author = "@COWL_VENDOR@"
+version = "@PROJECT_VERSION@"
+release = "@PROJECT_VERSION@"
+logo = "@COWL_ICON@"
+git_url = "@COWL_GIT_URL@"
+poliba_url = "http://www.poliba.it"
+sisinflab_url = "http://sisinflab.poliba.it"
+swot_url = "@COWL_VENDOR_URL@"
 
 # Sphinx
 
-primary_domain = 'c'
-default_role = 'any'
+primary_domain = "c"
+default_role = "any"
 toc_object_entries = False
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-extensions = ['breathe', 'sphinx.ext.intersphinx']
-rst_prolog = f':github_url: {git_url}'
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+extensions = ["breathe", "sphinx.ext.intersphinx"]
+rst_prolog = f":github_url: {git_url}"
 rst_epilog = (
-    f'.. _git_url: {git_url}\n'
-    f'.. |git_url| replace:: {git_url}.git\n'
-    f'.. _swot_url: {swot_url}\n'
-    f'.. _poliba_url: {poliba_url}\n'
+    f".. _git_url: {git_url}\n"
+    f".. |git_url| replace:: {git_url}.git\n"
+    f".. _swot_url: {swot_url}\n"
+    f".. _poliba_url: {poliba_url}\n"
 )
 
 # HTML
 
-html_theme = 'sphinx_rtd_theme'
-html_theme_options = {'logo_only': False}
-templates_path = ['@SPHINX_INPUT_DIRECTORY@/_templates']
-html_static_path = ['@SPHINX_INPUT_DIRECTORY@/_static', '@DOCS_IMAGES_DIRECTORY@']
-html_css_files = ['style.css']
+html_theme = "sphinx_rtd_theme"
+html_theme_options = {"logo_only": False}
+templates_path = ["@SPHINX_INPUT_DIRECTORY@/_templates"]
+html_static_path = ["@SPHINX_INPUT_DIRECTORY@/_static", "@DOCS_IMAGES_DIRECTORY@"]
+html_css_files = ["style.css"]
 html_logo = logo
-html_short_title = f'{project} docs'
-html_context = {
-    'poliba_url': poliba_url,
-    'sisinflab_url': sisinflab_url,
-    'swot_url': swot_url
-}
+html_short_title = f"{project} docs"
+html_context = {"poliba_url": poliba_url, "sisinflab_url": sisinflab_url, "swot_url": swot_url}
 html_copy_source = False
 html_show_sphinx = False
 html_use_index = False
@@ -47,14 +43,14 @@ html_use_index = False
 # Intersphinx
 
 intersphinx_mapping = {
-    'ulib': ('https://ivanobilenchi.com/docs/ulib', '@ULIB_DOCS_INVENTORY_FILE@')
+    "ulib": ("https://ivanobilenchi.com/docs/ulib", "@ULIB_DOCS_INVENTORY_FILE@")
 }
 
 # Breathe
 
-breathe_projects = {project: '@DOXYGEN_XML_OUTPUT_DIRECTORY@'}
+breathe_projects = {project: "@DOXYGEN_XML_OUTPUT_DIRECTORY@"}
 breathe_default_project = project
-breathe_default_members = ('members', 'undocmembers')
+breathe_default_members = ("members", "undocmembers")
 breathe_domain_by_extension = {
     "h": "c",
 }
