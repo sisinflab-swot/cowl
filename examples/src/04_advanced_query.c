a1ed7afe5189bc7aeff0503486d6b88a904ef2b4
Ivano Bilenchi <ivanobilenchi@gmail.com>
2026-06-05 17:26:20 +0200

    Update the advanced query example

--- examples/src/04_advanced_query.c
+++ examples/src/04_advanced_query.c
@@ -24,13 +24,6 @@
 #define CLASS_NAME "ThinAndCrispyBase"
 #define PROPERTY_NAME "hasBase"
 
-// Iterator body, invoked for each axiom matching the query.
-static cowl_ret for_each_axiom(void *stream, CowlAny *axiom) {
-    cowl_write(stream, axiom);
-    cowl_write_literal(stream, "\n");
-    return COWL_CONTINUE;
-}
-
 int main(void) {
     cowl_init();
 
@@ -45,20 +38,39 @@ int main(void) {
     CowlClass *cls = cowl_class_from_literal(NS CLASS_NAME);
     CowlObjProp *prop = cowl_obj_prop_from_literal(NS PROPERTY_NAME);
 
-    // We want to log all SubClassOf and EquivalentClasses axioms that reference
-    // both the class and the property.
+    // We want to log all SubClassOf and EquivalentClasses axioms that
+    // reference both the class and the property.
 
-    // Note that this can be done via other query functions as well, though
-    // using a CowlAxiomFilter is usually more efficient, as it is used
-    // internally to determine the best indexing strategy for the query.
+    // Note that this query can be done via other functions as well,
+    // though using a CowlAxiomFilter is usually more efficient, as it
+    // is used to determine the best indexing strategy for the query.
     CowlAxiomFlags types = COWL_AF_SUB_CLASS | COWL_AF_EQUIV_CLASSES;
     CowlAxiomFilter filter = cowl_axiom_filter(types);
     cowl_axiom_filter_add_primitive(&filter, cls);
     cowl_axiom_filter_add_primitive(&filter, prop);
 
-    CowlIterator iter = { uostream_std(), for_each_axiom };
+    // Other than using custom iterators, Cowl provides some built-in ones
+    // for common tasks. Here we use a set iterator: all axioms matching
+    // the filter will be collected in a set, removing any duplicates.
+    UHash(CowlObjectPtr) set = uhset(CowlObjectPtr);
+    CowlIterator iter = cowl_iterator_set(&set, false);
     cowl_ontology_iterate_axioms_matching(onto, &filter, &iter);
 
+    // [Optional] Adding the pizza prefix to the default prefix map
+    // allows for more concise IRIs when logging the axioms.
+    CowlPrefixMap *pm = cowl_get_prefix_map();
+    UString const prefix = ustring_literal("pizza");
+    UString const ns = ustring_literal(NS);
+    cowl_prefix_map_add_raw(pm, prefix, ns, false);
+
+    // We can now log the axioms by iterating over the set.
+    UOStream *stream = uostream_std();
+    uhash_foreach (CowlObjectPtr, &set, item) {
+        cowl_write(stream, *item.key);
+        cowl_write_literal(stream, "\n");
+    }
+
+    uhash_deinit(CowlObjectPtr, &set);
     cowl_release_all(cls, prop, onto);
     return EXIT_SUCCESS;
 }
